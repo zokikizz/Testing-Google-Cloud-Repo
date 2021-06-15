86cf0237200d8952f3eed44dcb7df9f978d14afe
zoran.pecic <zoran.pecic@gerniks.com>
2021-06-15 02:59:56 +0200

    change default db

--- backEnd/backEnd/settings.py
+++ backEnd/backEnd/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'backEnd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'travelApp',
+        'USER': 'admin',
+        'PASSWORD': 'adminadmin',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
